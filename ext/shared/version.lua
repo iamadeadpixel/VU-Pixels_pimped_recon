f9d1bf6c515b0b698f6c137593975f4ccdc6124f
Ton Muller <spatieman@gmail.com>
2023-04-06 07:01:27 +0200

    Add files via upload

--- ext/shared/version.lua
+++ ext/shared/version.lua
@@ -1,4 +1,10 @@
 localModVersion = "1.0.3";
 -- current version for mod.json
 -- do not forgot to update mod.json and version.lua if u sync with github !
--- reason: upgrade - patch - bugfix - new release - stable release - test
+-- reason: What ever you want to put in..
+
+Events:Subscribe('Player:Chat', function(player, recipientMask, message)
+    if message == "!bf4 recon" then
+      ChatManager:SendMessage("BF4 Recon mod: Version 1.0.3", player)
+    end
+end)
